8246589641d469ca4e81fddbac9ac670fa162f1c
William Rocha <william.bueno.rocha@gmail.com>
2022-01-16 15:16:53 -0300

    children component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Card from './components/layout/Card'
 import Aleatorio from './components/basicos/Aleatorio'
 import Fragmento from './components/basicos/Fragmento'
 import ComParametro from './components/basicos/ComParametro'
@@ -9,16 +10,25 @@ export default () => {
     return (
         <div id="app">
             <h1>Fundamentos React</h1>
-            <Aleatorio
-                min={0}
-                max={10} />
-            <Fragmento />
-            <ComParametro
-                titulo="Situacao Aluno"
-                aluno="Pedro"
-                nota={9.3}
-            />
-            <Primeiro></Primeiro>
+            <Card titulo="#04 - Exemplo de Card">
+                <Aleatorio min={0} max={10} />
+            </Card>
+
+            <Card titulo="#03 - Fragmento">
+                <Fragmento />
+            </Card>
+
+            <Card titulo="#02 - Com parametro">
+                <ComParametro
+                    titulo="Situacao Aluno"
+                    aluno="Pedro"
+                    nota={9.3}
+                />
+            </Card>
+
+            <Card titulo="#01 - Primeiro component">
+                <Primeiro></Primeiro>
+            </Card>
         </div>
     )
 }
