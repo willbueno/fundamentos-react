762958fc61e7af264fd37d856c86a2d17f2a11a2
William Rocha <william.bueno.rocha@gmail.com>
2022-01-15 23:04:44 -0300

    second component implementation

---
+++ src/components/basicos/ComParametro.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function (props) {
+    const status = props.nota >= 7 ? 'Aprovado' : 'Recuperacao'
+    return (
+        <div>
+            <h2>{props.titulo}</h2>
+            <p>
+                <strong>{props.aluno} </strong>
+                tem nota
+                <strong> {props.nota} </strong>
+                e esta
+                <strong> { status }</strong>!
+            </p>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <Primeiro></Primeiro>
+        <ComParametro
+            titulo="Situacao Aluno"
+            aluno="Pedro"
+            nota={9.3} />
     </div>,
     document.getElementById('root')
 )
