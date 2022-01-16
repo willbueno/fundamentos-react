e3066ac0822fd4ced608828103a94e9e835dffce
William Rocha <william.bueno.rocha@gmail.com>
2022-01-15 22:31:52 -0300

    first component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+import Primeiro from './components/basicos/Primeiro'
+
 const tag = <strong>Ola React</strong>
 
 ReactDOM.render(
     <div>
-        { tag }
+        <Primeiro></Primeiro>
     </div>,
     document.getElementById('root')
 )
