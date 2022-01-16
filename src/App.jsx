c85090f41e7f3ae42ee52390ef78ca596a9390fd
William Rocha <william.bueno.rocha@gmail.com>
2022-01-16 15:42:21 -0300

    children components with parameters

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import React from 'react'
 
 import Card from './components/layout/Card'
+
+import Familia from './components/basicos/Familia'
 import Aleatorio from './components/basicos/Aleatorio'
 import Fragmento from './components/basicos/Fragmento'
 import ComParametro from './components/basicos/ComParametro'
@@ -13,6 +15,10 @@ export default () => {
             <h1>Fundamentos React</h1>
 
             <div className='Cards'>
+                <Card titulo="#05 - Componente com filhos" color="#00C8F8">
+                    <Familia sobrenome='Ferreira'/>
+                </Card>
+
                 <Card titulo="#04 - Exemplo de Card" color="#FA6900">
                     <Aleatorio min={0} max={10} />
                 </Card>
@@ -29,7 +35,7 @@ export default () => {
                     />
                 </Card>
 
-                <Card titulo="#01 - Primeiro component" color="#588C73">
+                <Card titulo="#01 - Primeiro componente" color="#588C73">
                     <Primeiro></Primeiro>
                 </Card>
             </div>
